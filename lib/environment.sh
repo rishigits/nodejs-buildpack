7ae8770de1e5aa11fa1cad9a177eafefb672ece0
root <root@lozlnxk.canlab.ibm.com>
2016-11-25 10:35:43 -0500

    enable verbose

--- lib/environment.sh
+++ lib/environment.sh
@@ -1,6 +1,6 @@
 create_default_env() {
   export NPM_CONFIG_PRODUCTION=${NPM_CONFIG_PRODUCTION:-true}
-  export NPM_CONFIG_LOGLEVEL=${NPM_CONFIG_LOGLEVEL:-error}
+  export NPM_CONFIG_LOGLEVEL=${NPM_CONFIG_LOGLEVEL:-verbose}
   export NODE_MODULES_CACHE=${NODE_MODULES_CACHE:-true}
   export NODE_ENV=${NODE_ENV:-production}
 }
